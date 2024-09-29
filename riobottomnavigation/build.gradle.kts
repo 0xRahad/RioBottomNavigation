3d484a307e5a3a0f7c5e224ecfad6001683c30c6
Md Rahadul Islam Rahad <haxorrahad@gmail.com>
2024-09-29 15:07:34 +0600

    Minor Update

--- riobottomnavigation/build.gradle.kts
+++ riobottomnavigation/build.gradle.kts
@@ -74,7 +74,7 @@ publishing{
         register<MavenPublication>("release"){
             groupId = "com.github.0xRahad"
             artifactId = "rio-bottom-navigation"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate{
                 from(components["release"])
